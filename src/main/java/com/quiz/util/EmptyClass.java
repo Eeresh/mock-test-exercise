5bf0adb02b0afee41b165cfa45dac809e782b2f5
Eeresh M S <eereshms@gmail.com>
2020-01-28 21:30:20 +0530

    adding new file name EmptyClass

---
+++ src/main/java/com/quiz/util/EmptyClass.java
@@ -0,0 +1,20 @@
+package com.quiz.util;
+
+public class EmptyClass {
+
+	private String emptyName;
+	private int emptyId;
+	public String getEmptyName() {
+		return emptyName;
+	}
+	public void setEmptyName(String emptyName) {
+		this.emptyName = emptyName;
+	}
+	public int getEmptyId() {
+		return emptyId;
+	}
+	public void setEmptyId(int emptyId) {
+		this.emptyId = emptyId;
+	}
+
+}
